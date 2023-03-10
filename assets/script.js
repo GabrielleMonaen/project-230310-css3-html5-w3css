2e4771962310caf3a637949ea1b53686a29edc58
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-10 22:24:15 +0000

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,8 @@ fetch('https://api.github.com/users/GabrielleMonaen/repos')
         .then(response => response.json())
         .then(projectData => {
           const date = new Date(projectData.updated_at).toLocaleDateString();
-          const techList = projectData.language ? `<br><small><i>Technologies: ${projectData.language}</i></small>` : '';
+          //const techList = projectData.language ? `<br><small><i>Technologies: ${projectData.language}</i></small>` : '';
+          const techList = projectData.topics ? `<br><i>Technologies: ${projectData.topics.join(', ')}</i>` : '';
           
           // Create the card HTML with the project name, date, and technologies
           const card = `
